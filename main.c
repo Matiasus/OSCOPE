1ecd80cfb42f43da261f6ba35d796f0a50dba081
Marián <Matiasus@users.noreply.github.com>
2017-06-19 19:13:05 +0200

    Add files via upload

--- main.c
+++ main.c
@@ -11,14 +11,39 @@
  * @inspiration http://www.displayfuture.com/Display/datasheet/controller/ST7735.pdf
  * ----------------------------------------------------------------------------------
  */
+#include <util/delay.h>
 #include <avr/interrupt.h>
 #include "lib/st7735.h"
 #include "lib/auxillary.h"
 
 /**
+ * @description INT0 interrupt
+ *
+ * @param  INT0_vect
+ * @return Void
+ */
+ISR(INT0_vect) 
+{
+  // no axis
+  _axis = 0;
+}
+
+/**
+ * @description INT1 interrupt
+ *
+ * @param  INT1_vect
+ * @return Void
+ */
+ISR(INT1_vect) 
+{
+  // with axis
+  _axis = 1;
+}
+
+/**
  * @description ADC end converting
  *
- * @param ADC_vect
+ * @param  ADC_vect
  * @return Void
  */
 ISR(ADC_vect) 
